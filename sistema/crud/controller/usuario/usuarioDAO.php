e7d1bf82b5f312b2c5cd0c3e260872d960e3cb6d
matheussb15 <47834365+matheussb15@users.noreply.github.com>
2019-05-31 21:56:47 -0300

    EXCLUIR E LISTA - CRUD 31/05
    EXCLUIR E LISTA - CRUD 31/05

---
+++ sistema/crud/controller/usuario/usuarioDAO.php
@@ -0,0 +1,28 @@
+<?php
+
+	require_once ('../banco/conexao.php');
+
+	$atualizar = false;
+	$id=0;
+	$usuario = '';
+	$senha = '';
+
+	if(isset($_POST['salvar'])){
+		$usuario = $_POST['usuario'];
+		$senha = $_POST['senha'];
+
+		$conexao->query("INSERT INTO usuario (usuario,senha) VALUES ('$usuario','$senha')") or die($conexao->error);
+
+		header("Location: usuario.php");
+	}
+
+	if(isset($_GET['excluir'])){
+		$id = $_GET['excluir'];
+
+		$conexao->query("DELETE FROM usuario WHERE id=$id") or die($conexao->error);
+
+		header("Location: usuario.php");
+	}
+
+
+?>
